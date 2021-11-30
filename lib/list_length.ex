83e4b18c14800b5517df8269de74830a69bb43be
kukajp3 <kukajp@gmail.com>
2021-11-30 00:31:54 -0300

    completed challenge

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: length(list, 0)
+
+  defp length([], acc) do
+    acc
+  end
+
+  defp length([_ | tail], acc) do
+    acc = acc + 1
+    length(tail, acc)
   end
 end
